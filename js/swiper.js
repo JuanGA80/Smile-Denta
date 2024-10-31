4a3b4755139e788003804940239f6a835d13cd10
JuanGA80 <juangeap@gmail.com>
2024-10-31 15:51:35 -0600

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+
+});
